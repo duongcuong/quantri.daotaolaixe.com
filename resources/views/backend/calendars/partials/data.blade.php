5f282ac83153bc243c7ade275e7b715b98a592e3
duongcuong <duongdinhcuong.20121992@gmail.com>
2025-03-03 00:46:49 +0700

    Update

--- resources/views/backend/calendars/partials/data.blade.php
+++ resources/views/backend/calendars/partials/data.blade.php
@@ -182,7 +182,7 @@ $columns = request()->has('show_column') ? explode(',', request()->show_column) 
             @endif
 
             @if (!request()->has('show_column') || in_array('health_check_date', $columns))
-            <td>{{ $calendar->courseUser->toArray() }}</td>
+            <td>{{ $calendar->courseUser }}</td>
             @endif
 
             @if (!request()->has('show_column') || in_array('loai_hoc', $columns))
