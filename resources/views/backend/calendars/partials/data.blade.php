0910acff2e333c11e862a974cf3d8fb5743e99c7
duongcuong <duongdinhcuong.20121992@gmail.com>
2025-03-03 00:43:46 +0700

    Update

--- resources/views/backend/calendars/partials/data.blade.php
+++ resources/views/backend/calendars/partials/data.blade.php
@@ -182,7 +182,7 @@ $columns = request()->has('show_column') ? explode(',', request()->show_column) 
             @endif
 
             @if (!request()->has('show_column') || in_array('health_check_date', $columns))
-            <td>{!! getDateTimeStamp($calendar->courseUser->health_check_date, 'd/m/Y') ?? "" !!}</td>
+            <td><?php print_r($calendar->courseUser) ?></td>
             @endif
 
             @if (!request()->has('show_column') || in_array('loai_hoc', $columns))
